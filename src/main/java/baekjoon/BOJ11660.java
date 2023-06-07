c93784c5fbdc513766af4b037e0312177cee09bd
Younghoon Kim <the_basic_@naver.com>
2023-06-07 23:49:18 +0900

    2023년 6월 7일 문제 풀이

---
+++ src/main/java/baekjoon/BOJ11660.java
@@ -0,0 +1,44 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ11660 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] maps = new int[n + 1][n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= n; j++) {
+                maps[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                maps[i][j] = maps[i][j - 1] + maps[i - 1][j] - maps[i - 1][j - 1] + maps[i][j];
+            }
+        }
+
+        for (int i = 1; i <= m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            long res = 0;
+            res = maps[x2][y2] - maps[x2][y1 - 1] - maps[x1 - 1][y2] + maps[x1 - 1][y1 - 1];
+            System.out.println(res);
+        }
+    }
+}
